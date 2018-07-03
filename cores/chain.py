d57a837d7bf7d577b95234ad09fbf9fc1d7deca5
Adrian Schneider <github@ardy.io>
2018-07-03 10:24:10 +1000

    add basic processing chain

--- cores/chain.py
+++ cores/chain.py
@@ -3,11 +3,26 @@ from migen import *
 from convert_data import ConvertData
 from hispi_decoder import HispiDecoder
 
+class Chain(Module):
+    """ Move data from one part of the processing chain to the next"""
+    def __init__(self):
+        self.submodules.converter = ConvertData()
+        self.submodules.decoder = HispiDecoder()
 
-def test_decode_frame():
+        self.in_data = self.converter.in_data
+        self.out_data = self.decoder.out
 
+        # Use converter clk_out to tell us when data is ready to be moved out
+        cd = ClockDomain(reset_less = True, name="hispi")
+        cd.clk = self.converter.clk_out
+        self.clock_domains += cd
+
+        self.sync.hispi += [self.decoder.in_data.eq(self.converter.output)]
+
+
+def test_clock_division():
     word = 0
-    def testbench(decoder):
+    def testbench():
         f = open("test_data/test_convert.txt")
         i = 0
         frame_start = False
@@ -17,15 +32,11 @@ def test_decode_frame():
             if i > 10000:
                 return
 
-            yield converter.in_data.eq(int(line.strip(), 2))
-            yield
+            yield dut.in_data.eq(int(line.strip(), 2))
             if i % 2 == 0:
-                word = (yield converter.output)
-                yield decoder.in_data.eq(word)
-                yield
-                assert (yield decoder.in_data) == word
+                word = (yield dut.converter.output)
+                assert (yield dut.decoder.in_data) == word
 
 
-    decoder = HispiDecoder()
-    converter = ConvertData()
-    run_simulation(converter, testbench(decoder))
+    dut = Chain()
+    run_simulation(dut, testbench())
