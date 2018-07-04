ff82efe5fdb1ff3ab7f1ee5cb456460e998c7403
Adrian Schneider <github@ardy.io>
2018-07-04 18:57:53 +1000

    add debug printing to test clock timing

--- cores/chain.py
+++ cores/chain.py
@@ -11,6 +11,8 @@ class Chain(Module):
 
         self.in_data = self.converter.in_data
         self.out_data = self.decoder.out
+        
+        self.t = Signal(max=100)
 
         # Use converter clk_out to tell us when data is ready to be moved out
         self.clock_domains.cd_hispi = ClockDomain(reset_less=True)
@@ -18,27 +20,38 @@ class Chain(Module):
         self.comb += self.cd_hispi.clk.eq(self.converter.clk_out)
         self.sync.hispi += [self.decoder.in_data.eq(self.converter.output)]
 
+        self.sync.hispi += [self.t.eq(self.t + 1)]
+
 
 def test_clock_division():
     def testbench():
         f = open("test_data/test_convert.txt")
-        i = 0
+        i = 1
         previous_data = 0
 
         for line in f:
             i += 1
-            if i > 10000:
-                return
+            if i > 10:
+                break
 
+            # import pdb; pdb.set_trace()
             yield dut.in_data.eq(int(line.strip(), 2))
             yield
 
             # new data is moved in every second clock cycle
+            print("conv out: ", format((yield dut.converter.output), 'b'))
+            print("dec in: ", format((yield dut.decoder.in_data), 'b'))
+            print("clock out: ", (yield dut.converter.clk_out))
+            print("clock in counter: ", (yield dut.t))
+            print()
             if i % 2 == 0:
                 word = (yield dut.converter.output)
                 assert (yield dut.decoder.in_data) == word
             else:
-                assert (yield dut.decoder.in_data) == previous_data
+                pass
+                # assert (yield dut.decoder.in_data) == previous_data
+
+        assert False
 
 
     dut = Chain()
@@ -46,26 +59,39 @@ def test_clock_division():
 
 
 def test_decoding():
+    return
+    import PIL
     def testbench():
         f = open("test_data/test_convert.txt")
         i = 0
 
         valid = False
         frame_start = False
+        frame = b""
 
         for line in f:
             i += 1
             if i > 10000:
-                return
+                break
 
             yield dut.in_data.eq(int(line.strip(), 2))
+            yield
+            print((yield dut.converter.output))
 
             if (yield dut.decoder.data_valid) == 1:
                 valid = True
             if (yield dut.decoder.frame_start) == 1:
+                if frame_start == True:
+                    break
                 frame_start = True
 
-        assert valid == true and frame_start == True
+            if frame_start:
+                frame += dut.decoder.out
+
+        assert valid == True and frame_start == True
+
+        img = PIL.Image.frombytes("L", (1080, 720), frame)
+        img.save("test.png")
 
     dut = Chain()
     run_simulation(dut, testbench())
