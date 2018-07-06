ab19a8611ef28d86cb676460d6a97ed7422deaa6
Adrian Schneider <github@ardy.io>
2018-07-06 13:33:57 +1000

    remove debug code

--- cores/chain.py
+++ cores/chain.py
@@ -12,15 +12,11 @@ class Chain(Module):
         self.in_data = self.converter.in_data
         self.out_data = self.decoder.out
         
-        self.t = Signal(max=100)
-
         # Use converter clk_out to tell us when data is ready to be moved out
         self.clock_domains.cd_hispi = ClockDomain(reset_less=True)
 
         self.comb += self.cd_hispi.clk.eq(self.converter.clk_out)
         self.sync.hispi += [self.decoder.in_data.eq(self.converter.output)]
-
-        self.sync.hispi += [self.t.eq(self.t + 1)]
 
 
 def test_clock_division():
@@ -34,16 +30,10 @@ def test_clock_division():
             if i > 1000:
                 break
 
-            # import pdb; pdb.set_trace()
             yield dut.in_data.eq(int(line.strip(), 2))
             yield
 
             # new data is moved in every second clock cycle
-            print("conv out: ", format((yield dut.converter.output), 'b'))
-            print("dec in: ", format((yield dut.decoder.in_data), 'b'))
-            print("clock out: ", (yield dut.converter.clk_out))
-            print("clock in counter: ", (yield dut.t))
-            print()
             if i % 2 == 0:
                 word = (yield dut.converter.output)
                 assert (yield dut.decoder.in_data) == word
