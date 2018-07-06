a186691343f915974148a066086c95d6fb1bddb0
Adrian Schneider <github@ardy.io>
2018-07-06 20:43:16 +1000

    use MultiReg for clock domain crossing
    Due to this, the HispiDecoder input lags further behind the ConvertData
    output, hence the test has to compare to the input one cycle back and
    two cycles back to check the input only gets changed every other cycle

--- cores/chain.py
+++ cores/chain.py
@@ -1,13 +1,14 @@
 from migen import *
+from migen.genlib.cdc import MultiReg
 
 from convert_data import ConvertData
 from hispi_decoder import HispiDecoder
 
 class Chain(Module):
     """ Move data from one part of the processing chain to the next"""
-    def __init__(self):
+    def __init__(self, num_lanes=4, hispi_bits=12):
         self.submodules.converter = ConvertData()
-        self.submodules.decoder = ClockDomainsRenamer("hispi")(HispiDecoder())
+        self.submodules.decoder = ClockDomainsRenamer("hispi")(HispiDecoder(num_lanes=num_lanes, hispi_bits=hispi_bits))
 
         self.in_data = self.converter.in_data
         self.out_data = self.decoder.out
@@ -15,8 +16,11 @@ class Chain(Module):
         # Use converter clk_out to tell us when data is ready to be moved out
         self.clock_domains.cd_hispi = ClockDomain(reset_less=True)
 
+        conv_out_hispi = Signal(num_lanes * hispi_bits)
+        self.specials += MultiReg(self.converter.output, conv_out_hispi, "hispi")
+
         self.comb += self.cd_hispi.clk.eq(self.converter.clk_out)
-        self.sync.hispi += [self.decoder.in_data.eq(self.converter.output)]
+        self.comb += [self.decoder.in_data.eq(conv_out_hispi)]
 
 
 def test_clock_division():
@@ -24,6 +28,7 @@ def test_clock_division():
         f = open("test_data/test_convert.txt")
         i = 1
         previous_data = 0
+        prev2_data = 0
 
         for line in f:
             i += 1
@@ -36,11 +41,11 @@ def test_clock_division():
             # new data is moved in every second clock cycle
             if i % 2 == 0:
                 word = (yield dut.converter.output)
-                assert (yield dut.decoder.in_data) == word
+                assert (yield dut.decoder.in_data) == previous_data
+                prev2_data = previous_data
                 previous_data = word
             else:
-                pass
-                assert (yield dut.decoder.in_data) == previous_data
+                assert (yield dut.decoder.in_data) == prev2_data
 
     dut = Chain()
     run_simulation(dut, testbench(), clocks = {'sys': 10, 'hispi': 20})
@@ -57,7 +62,7 @@ def test_decoding():
 
         for line in f:
             i += 1
-            if i > 10000:
+            if i > 1000:
                 break
 
             yield dut.in_data.eq(int(line.strip(), 2))
@@ -80,4 +85,4 @@ def test_decoding():
         assert valid == True and frame_start == True
 
     dut = Chain()
-    run_simulation(dut, testbench(), clocks = {'sys': 10, 'hispi': (20, 10)})
+    run_simulation(dut, testbench(), clocks = {'sys': 10, 'hispi': (20, 10)}, vcd_name="decoding.vcd")
